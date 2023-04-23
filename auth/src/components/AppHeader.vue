7f101886bc865aabd124fbcaa2cac65291c0891d
Ikotun Collins <danlogan2003@gmail.com>
2023-04-23 17:13:55 +0100

    tailwind config

---
+++ auth/src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Hoere.</h1>
+</template>
+
+<script>
+
+export default{
+    name: 'AppHeader'
+}
+
+</script>
---
+++ auth/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+
+    extend: {
+      colors: {
+        primary: '#FF0000',
+        secondary: '#00FF00',
+        tertiary: '#0000FF',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      spacing: {
+        '96': '24rem',
+        '128': '32rem',
+      },
+
+
+    },
+  },
+  plugins: [],
+}
