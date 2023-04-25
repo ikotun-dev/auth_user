28c8306befaebe27749014dcde976e6da932157c
Ikotun Collins <danlogan2003@gmail.com>
2023-04-25 21:31:44 +0100

    router

--- auth/src/main.js
+++ auth/src/main.js
@@ -1,27 +1,45 @@
+// import App from './App.vue'
+// import './assets/tailwind.css'
+
+// import Vue from 'vue'
+// import VueRouter from 'vue-router'
+// import LoginForm from './components/LoginForm.vue'
+// import DashBoard from './components/DashBoard.vue'
+
+// Vue.use(VueRouter)
+
+// const routes = [
+//     { path: '/', component: LoginForm },
+//     { path: '/dashboard', component: DashBoard }
+// ]
+
+// const router = new VueRouter({
+//     mode: 'history',
+//     routes
+// })
+
+// new Vue({
+//     router,
+//     render: h => h(App),
+// }).$mount('#app')
+
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
 
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import App from './App.vue'
-import Login from './components/Login.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginForm from './components/LoginForm.vue'
 import DashBoard from './components/DashBoard.vue'
 
-Vue.use(VueRouter)
-
 const routes = [
-    { path: '/', component: Login },
+    { path: '/', component: LoginForm },
     { path: '/dashboard', component: DashBoard }
 ]
 
-const router = new VueRouter({
-    mode: 'history',
+const router = createRouter({
+    history: createWebHistory(),
     routes
 })
 
-new Vue({
-    router,
-    render: h => h(App),
-}).$mount('#app')
-
+createApp(App).use(router).mount('#app')
